3d7fce789992e200bd6096990dfc859bcdc705ca
Jakob Bauers <jakob@airtame.com>
2018-12-18 22:06:06 +0100

    Sort index by date modified, make it default

--- doc-docs.sh
+++ doc-docs.sh
@@ -48,13 +48,24 @@ time find . -type f -iname "*.md" -print0 | while IFS= read -r -d $'\0' file; do
 
     create_html_file $source_file ${OUT_DIR}
 
+    # Replace the block between EOF below with the following line to sort alphabetically.
+    # You also don't need 'modified_date' if you do so:
+    # [${source_file%.md}.html](${source_file%.md}.html)
+    modified_date=`stat -c %y $source_file | awk -F ' ' '{print $1}'`
     cat <<EOF >> $tmplinks
-[${source_file%.md}.html](${source_file%.md}.html)  
+$modified_date<DEL>[${source_file%.md}.html](${source_file%.md}.html) - $modified_date<br>
 EOF
 done
 
 cat ${INDEX_HEAD} > $tmpindex
-cat $tmplinks | sort >> $tmpindex && rm $tmplinks
+
+# Replace the following block with the following command to sort alphabetically:
+# cat $tmplinks | sort >> $tmpindex && rm $tmplinks
+cat $tmplinks |
+sort -r -b -k 1.1,1.4 -k 1.6,1.7 -k 1.9,1.10 |
+awk -F '<DEL>' '{print $2}' \
+>> $tmpindex && rm $tmplinks
+
 cat ${INDEX_TAIL} >> $tmpindex
 
 create_html_file $tmpindex ${OUT_DIR}
